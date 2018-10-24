ff50b819a1835f3d36a7d9e66f8af2a2e44201e6
dfimiarz <dfimiarz@ccny.cuny.edu>
2018-10-23 23:12:13 -0400

    More work on booking module.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,46 +1,45 @@
 
 <template>
-<v-container fluid fill-height="">
-  <v-layout justify-center align-content-space-around="" wrap="">
+<v-container fluid fill-height="" >
+  <v-layout justify-center align-content-space-between="" wrap="">
     <v-flex  md12 lg8 x6>
-      <!--
-        <div class="main-title text-xs-center">
-            Knickerbocker Tennis
-        </div>
-        -->
       <v-card raised="" >
         <v-card-title primary-title>
-          <div>
-            <div class="main-title text-xs-center">
-              Knickerbocker Tennis
-            </div>
-            <div>Welcome!. Please choose one of the options below...</div>
-          </div>
+          <v-container fluid="">
+            <v-layout wrap="">
+              <v-flex xs12>
+                <div class="main-title text-xs-center">
+                  Knickerbocker Tennis
+                </div>
+              </v-flex>
+              <v-flex xs12>
+                <div class="main-text text-xs-center">
+                  Please choose one of the options below...
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card-title>
-        
       </v-card>
     </v-flex>
     <v-flex md12 lg8 x6>
-        <v-container
-          fluid
-          grid-list-lg
-        >
+        <v-container fluid="" grid-list-lg >
           <v-layout row wrap>
             <v-flex
               v-for="card in cards"
-              v-bind="{ [`xs${card.flex}`]: true }"
+              v-bind="{ [`xs${card.flexxs}`]: true,
+                        [`sm${card.flexsm}`]: true }"
               :key="card.title"
             >
               <v-card :to="{name: 'NewBooking'}" raised="" >
                 <v-img
                   :src="card.src"
-                  height="250px"
-                  
+                  height="250px"  
                 >
                   
                   <v-container fill-height fluid>
                     <v-layout justify-center="" align-end="" row>
-                      <v-flex xs12 flexbox>
+                      <v-flex xs12>
                         <div class='tile-text-container'>
                           <h3 class="tile-title">{{ card.title }}</h3>
                           <div>{{ card.text }}</div>
@@ -64,10 +63,10 @@ export default {
   data() {
     return {
       cards: [
-        { title: 'Matches', text: 'View today\'s match schedule' ,src: './Matches.jpg', flex: 6 },
-        { title: 'Booking', text: 'Reserve a court', src: './Booking700x325.jpg', flex: 6 },
-        { title: 'Club Admin', text: 'Administrative features',  src: './Manage.jpg', flex: 6 },
-        { title: 'Club Info', text: 'Clud information', src: './Manage.jpg', flex: 6 }
+        { title: 'Matches', text: 'View today\'s match schedule' ,src: './Matches.jpg', flexxs: 12, flexsm: 6 },
+        { title: 'Booking', text: 'Reserve a court', src: './Booking700x325.jpg', flexxs: 12, flexsm: 6 },
+        { title: 'Club Admin', text: 'Administrative features',  src: './Manage.jpg', flexxs: 12, flexsm: 6 },
+        { title: 'Club Info', text: 'Clud information', src: './Manage.jpg', flexxs: 12, flexsm: 6 }
       ]
       
     }
@@ -97,6 +96,10 @@ export default {
 }
 
 .main-title{
-  font-size: 5vmax;
+  font-size: 4vmax;
+}
+
+.main-text{
+  font-size: 1vmax;
 }
 </style>
