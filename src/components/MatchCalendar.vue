f9bb432df839ba10212f614724754bebfc75f966
dfimiarz <dfimiarz@ccny.cuny.edu>
2018-11-01 10:30:20 -0400

    Fixed issues with court grid overflowing on mobile

--- src/components/MatchCalendar.vue
+++ src/components/MatchCalendar.vue
@@ -7,64 +7,46 @@ import func from './vue-temp/vue-editor-bridge';
       Date: 2018/10/01
     </v-flex>
     <v-flex xs12 lg10 >
-        <div class="main-schedule-container" ref="scheduleContainer" @click="contClicked($event)">
-          <div class="court-grid-container">
-            <div v-for="(court,index) in courts" :key="court.id" v-bind:style="{ 'grid-column' : index + 2, 'grid-row' : 1 }">
-              <v-container fluid="" fill-height="">
-                <v-layout row wrap align-center="" justify-center="">
-                  <v-flex xs12>
-                    <div class="text-xs-center" style=" font-size: 2vmax;">
-                      {{ court.lbl }}    
-                    </div>
-                    <div class="title text-xs-center">
-                      <span class="green darken-3 px-2" style=" font-size: 1.5vmax;">{{ court.status }}</span>
-                    </div>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-              
-            </div>
-            <v-btn
-              absolute
-              fab
-              bottom
-              right
-              color="pink"
-            >
-              <v-icon>add</v-icon>
-            </v-btn>
-          </div>
-          <div style="grid-column : 1 ; grid-row : 2;" class="time-grid-container">
-              
-              <div v-for="(n) in totalCellCount" :key="n" class="cell"  v-bind:style="{ height:  + cellHeight1H + 'px' }">
-                  {{ getCellLabel(n) }}
-              </div>
-              <div class="session-grid-container">
-                <div style="grid-column : 2 ; grid-row : 1; height: 1360px ; border: 1px solid; position: relative;"> 
-                  <match v-for="match in matches" :key="match.id" :vpos="(match.startMin * cellHeight1H/60)" :height="(match.durMin * cellHeight1H/60)"></match>
+     
+          <div class="main-schedule-container" ref="scheduleContainer" @click="contClicked($event)">
+            <div class="court-grid-container">
+              <div v-for="(court,index) in courts" :key="court.id" class="pa-1" v-bind:style="{ 'grid-column' : index + 2, 'grid-row' : 1 }">
+                <div class="text-xs-center" style=" font-size: 2vw;">
+                  {{ court.lbl }}    
                 </div>
-                <div style="grid-column : 3 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
-                <div style="grid-column : 4 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
-                <div style="grid-column : 5 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
-                <div style="grid-column : 6 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
+                <div class="title text-xs-center">
+                  <span class="green darken-3 px-2" style=" font-size: 1.5vw;">{{ court.status }}</span>
+                </div>
               </div>
-          </div>  
-        </div>
+            </div>
+            <div class="time-grid-container">
+                <div v-for="(n) in totalCellCount" :key="n" class="cell"  v-bind:style="{ height:  + cellHeight1H + 'px' }">
+                    {{ getCellLabel(n) }}
+                </div>
+                <div class="session-grid-container">
+                  <div style="grid-column : 2 ; grid-row : 1; height: 720px ; border: 1px solid; position: relative;">
+                    <match v-for="match in matches" :key="match.id" :vpos="(match.startMin * cellHeight1H/60)" :height="(match.durMin * cellHeight1H/60)"></match>
+                  </div>
+                  <div style="grid-column : 3 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
+                  <div style="grid-column : 4 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
+                  <div style="grid-column : 5 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
+                  <div style="grid-column : 6 ; grid-row : 1; height: 100%; border: 1px solid;"> </div>
+                </div>
+                
+            </div> 
+           
+          </div>
+           <v-btn
+                fixed=""
+                fab
+                bottom
+                right
+                color="pink"
+              >
+                <v-icon>add</v-icon>
+              </v-btn> 
         
     </v-flex>
-    <!--
-    <v-flex xs2 >
-      <div class="schedule-container" ref="scheduleContainer" @click="contClicked($event)">
-        <div v-for="n in civTime" :key="n" class="cell">
-          
-        </div>
-        <div v-for="session in sessions" :key="session.id" class="session" :style="{ height: session.durMin + 'px', top: session.startMin + 'px' }">
-          
-          <div>{{session.text}}</div>
-
-        </div>
-      </div>
-    </v-flex>-->
   </v-layout>
 </v-container>
 </template>
@@ -84,9 +66,9 @@ export default {
       milTimeLabels: [ '12', '1' , '2', '3', '4' , '5' , '6' , '7' , '8', '9' , '10' , '11', '12' , '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23' ],
       civTimeLabels: [ '12 am', '1 am' , '2 am', '3 am', '4 am' , '5 am' , '6 am' , '7 am' , '8 am', '9 am' , '10 am' , '11 am', '12 pm' , '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', '9 pm', '10 pm', '11 pm' ],
       courts: [ {id: 1, lbl:"Court #1", status: "OPEN"} , {id : 2,lbl:"Court #2", status: "OPEN"},  {id: 3,lbl:"Court #3", status: "OPEN"} , {id : 4,lbl:"Court #4", status: "OPEN"} , {id : 5, lbl:"Court #5", status: "OPEN"} ],
-      scheduleStartTime: 7,
-      scheduleEndTime: 22,
-      cellHeight1H: 90
+      scheduleStartTime: 8,
+      scheduleEndTime: 20,
+      cellHeight1H: 60
     }
   },
   methods: {
@@ -127,23 +109,25 @@ export default {
   user-select: none;
   position: relative;
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: auto;
   grid-template-rows: 1fr 650px;
   
 }
 
 .court-grid-container{
   position: relative;
-  height: 100%;
   display: grid;
   grid-template-columns: 40px repeat(5,1fr);
   grid-template-rows: 1fr;
   border: 1px solid;
+  height: 100%;
 }
 
 .time-grid-container{
   height: 100%;
   position: relative;
+  grid-column : 1;
+  grid-row : 2;
   overflow: auto;
   
   
