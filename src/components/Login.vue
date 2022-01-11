e2b44c9da9cfa627f00840d54f948165f74c5e13
Daniel Fimiarz <dfimiarz@gmail.com>
2022-01-10 19:07:00 -0500

    Changes to log in screen

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-container class="fill-height">
-    <v-row justify="center" no-gutters>
-      <v-col cols="12" md="6" lg="4" xl="3">
+  <v-container fluid class="fill-height">
+    <v-row justify="center" align-sm="end">
+      <v-col cols="12" sm="6" md="4" xl="2">
         <v-card>
           <v-card-subtitle>Log in</v-card-subtitle>
           <v-card-text>
             <v-form v-model="formvalid" ref="form">
               <v-row no-gutters>
                 <v-col cols="12">
+                  <v-img :src="require(`@/assets/clublogo.png`)"></v-img>
+                </v-col>
+                <v-col cols="12">
                   <v-text-field
-                    label="Email"
-                    outlined
+                    label="E-mail"
                     v-model="email"
                     :rules="[rules.required, rules.isEmail]"
                   ></v-text-field>
@@ -18,25 +20,25 @@
                 <v-col cols="12">
                   <v-text-field
                     label="Password"
-                    outlined
                     v-model="password"
                     :append-icon="showpassword ? eyeIcon : eyeOffIcon"
                     :type="showpassword ? 'text' : 'password'"
                     :rules="[rules.required, rules.min]"
                     @click:append="showpassword = !showpassword"
                     counter=""
-                    hint="At least 8 characters"
+                    hint="Minimum 8 characters"
                   ></v-text-field>
                 </v-col>
               </v-row>
             </v-form>
           </v-card-text>
           <v-card-actions>
-            <v-btn text @click="$refs.form.reset()">Clear</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn large @click="login">Login</v-btn>
+            <!-- <v-btn text @click="$refs.form.reset()">Clear</v-btn>
+            <v-spacer></v-spacer> -->
+            <v-btn large block @click="login">Login</v-btn>
           </v-card-actions>
         </v-card>
+         
       </v-col>
     </v-row>
   </v-container>
