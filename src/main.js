2e29186b02b7608a54f3ccc6a50b21ed80af4e58
Daniel Fimiarz <dfimiarz@gmail.com>
2022-06-05 17:29:30 -0400

    Added visibility check

--- src/main.js
+++ src/main.js
@@ -45,6 +45,29 @@ function initializeVueApp() {
 //   loadImages(img)
 // })
 
+var hidden, visibilityChange;
+if (typeof document.hidden !== "undefined") {
+  // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+
+function handleVisibilityChange() {
+  if (document[hidden]) {
+    store.dispatch("setBrowserInActive");
+  } else {
+    store.dispatch("setBrowserActive");
+  }
+}
+
+document.addEventListener(visibilityChange, handleVisibilityChange, false);
+
 store
   .dispatch("checkConnection")
   .then(() => {
