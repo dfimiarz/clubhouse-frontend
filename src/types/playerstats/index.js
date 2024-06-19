b0ab6ac4f667bb0160860136e83b631d4bc18814
Daniel Fimiarz <dfimiarz@gmail.com>
2024-06-19 11:05:42 -0400

    Added PlayerStats type for jsdoc

---
+++ src/types/playerstats/index.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {object} PlayerStats
+ * @property {String} date Date in YYYY-MM-DD format
+ * @property {Number} time_played Total time played in minutes
+ * @property {Number} player_count Player count
+ */
+
+module.exports = {};
