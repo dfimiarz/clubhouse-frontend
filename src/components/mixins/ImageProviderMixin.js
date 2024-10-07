4cdb1044831511e43257f3da41966ac8d4f5519d
Daniel Fimiarz <dfimiarz@gmail.com>
2024-10-07 09:17:31 -0400

    Add type info for image.

--- src/components/mixins/ImageProviderMixin.js
+++ src/components/mixins/ImageProviderMixin.js
@@ -1,17 +1,22 @@
 export const ImageProvider = {
     methods: {
         /**
-         * 
-         * @param {String} name 
-         * @returns {String} - The image src or null if the image is not found
+         * Get the full image URL by name
+         * @param {string} name Image name
+         * @returns {string|null} The full image URL or null if the image is not found
          */
-        getImageSrcByName: function (name) {
+        getImageSrcByName(name) {
             /**
-             * @type {name: string, src: string}
+             * @type {{name: string, src: string} | undefined}
              */
             const image = this.$store.getters['getImageByName'](name);
-            //Return the image.src if src is define, null otherwise
-            return image ? `${this.$store.state.cdn}/${image.src}` : null;
+            
+            if (!image || !image.src) {
+                return null;
+            }
+
+            return `${this.$store.state.cdn}/${image.src}`;
         }
     },
 };
+
